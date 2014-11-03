af05302045b4d97d9b8392ef4a4e20e5dba481e1
Kintoki <s12113240@stu.edu.tw>
2014-11-04 03:51:00 +0800

    update

--- insert.php
+++ insert.php
@@ -1,8 +1,8 @@
 <?php
 header("Content-Type:application/json;");
 require_once('loader.php');
-$sql = "INSERT INTO articlev VALUES(?,?,?,?,?,?,?,?,?)";
-$ans = sql_i($sql,array(NULL,$_POST['city'],$_POST['area'],$_POST['category'],$_POST['picture'],$_POST['title'],$_POST['subtitle'],$_POST['content'],$_POST['x'],$_POST['y'],'1'));
+$sql = "INSERT INTO article('city','area','category','picture','title','subtitle','content','x','y') VALUES(?,?,?,?,?,?,?,?,?)";
+$ans = sql_i($sql,array($_POST['city'],$_POST['area'],$_POST['category'],$_POST['picture'],$_POST['title'],$_POST['subtitle'],$_POST['content'],$_POST['x'],$_POST['y']));
 
 if($ans)
 	echo 'success';
