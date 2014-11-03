4e90c8247c10fa3d2b52a194502df3e780ea902b
Kintoki <s12113240@stu.edu.tw>
2014-11-04 03:22:58 +0800

    update

--- fincity.php
+++ fincity.php
@@ -1,8 +1,10 @@
 <?php
-header("Content-Type:application/json; charset=utf-8");
+header("Content-Type:application/json; ");
 require_once('loader.php');
 
-$ans = sql_q("SELECT * FROM article");
+$ans = sql_q("SELECT * FROM article WHERE city=? AND area=?",array( $_POST['city']),$_POST['area']));
 $sum = array('map'=>$ans);
 
 echo json_encode($sum);
+
+
