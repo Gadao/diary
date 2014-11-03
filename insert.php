15444a2be8691d0ae9793d48b05f9cdc86fe236e
Kintoki <s12113240@stu.edu.tw>
2014-11-04 04:04:01 +0800

    update

--- insert.php
+++ insert.php
@@ -1,6 +1,6 @@
 <?php
 header("Content-Type:text/html; charset=utf-8");
 require_once('loader.php');
-$sql = "INSERT INTO article(city,area,category,picture,title,subtitle,content,x,y) VALUES("")";
+$sql = "INSERT INTO article(city,area,category,picture,title,subtitle,content,x,y) ";
 
 echo $sql;
