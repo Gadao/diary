3963f80bd9aa6c827428636eef8d731a2dfe22c8
Kintoki <s12113240@stu.edu.tw>
2014-11-04 03:46:16 +0800

    update

--- insert.php
+++ insert.php
@@ -2,4 +2,9 @@
 header("Content-Type:application/json;");
 require_once('loader.php');
 $sql = "INSERT INTO article('no','city','area','category','picture','title','subtitle','content','x','y','mid') VALUES(?,?,?,?,?,?,?,?,?)";
-sql_i($sql,array(NULL,$_POST['city'],$_POST['area'],$_POST['category'],$_POST['picture'],$_POST['title'],$_POST['subtitle'],$_POST['content'],$_POST['x'],$_POST['y'],'1'));
+$ans = sql_i($sql,array(NULL,$_POST['city'],$_POST['area'],$_POST['category'],$_POST['picture'],$_POST['title'],$_POST['subtitle'],$_POST['content'],$_POST['x'],$_POST['y'],'1'));
+
+if($ans)
+	echo 'success';
+else
+	echo 'default';
