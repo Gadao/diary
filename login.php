c78b98b1f04808ac8e79e0599feba32277be321b
Kintoki <s12113240@stu.edu.tw>
2014-11-04 03:16:53 +0800

    update

--- login.php
+++ login.php
@@ -1,8 +1,8 @@
 <?php
-header("Content-Type:application/json; charset=utf-8");
+header("Content-Type:application/json; ");
 require_once('loader.php');
-$default = array('map'=>array('fail'=>'登入失敗');
-$ans = sql_q("SELECT * FROM member WHERE account =? AND password=?",array($_POST['account'],$_POST['password']));
+$default = array('map'=>array('fail'=>'登入失敗'));
+$ans = sql_q("SELECT * FROM member WHERE account =? AND password=?",array( $_POST['account'],$_POST['password'] ));
 $sum = array('map'=>$ans);
 if(count($ans)>0)
     echo json_encode($sum);
