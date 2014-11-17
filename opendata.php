0e3dc0bc203b3af5fe6695a217d32a0179f00e02
Kintoki <s12113240@stu.edu.tw>
2014-11-17 22:50:09 +0800

    update

--- opendata.php
+++ opendata.php
@@ -4,10 +4,10 @@ require_once('loader.php');
 $food_url = json_decode(file_get_contents( "http://data.coa.gov.tw/Service/OpenData/CC/FoodData.aspx" ),true);
 
 $opendata = array();
-// for ($i=0; $i < count($food_url) ; $i++) { 
-// 	unset($food_url[$i]['sc_id']);
-// }
- unset($food_url['sc_id']);
-echo json_encode($food_url[0]);
+for ($i=0; $i < count($food_url) ; $i++) {  	
+ 	unset($food_url[$i]['sc_id']);
+}
+
+echo json_encode($food_url);
 
 
