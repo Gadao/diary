187edfded12170293f46ffee2d717c5c2beb7be0
Kintoki <s12113240@stu.edu.tw>
2014-11-17 23:18:21 +0800

    update

--- opendata_weather.php
+++ opendata_weather.php
@@ -4,10 +4,17 @@ require_once('loader.php');
 $weather_url = json_decode(file_get_contents( "http://api.openweathermap.org/data/2.5/weather?lat=".$_GET['lat']."&lon=".$_GET['lon'] ),true);
 
 $opendata = array();
-foreach ($weather_url as $key => $value) {
-	echo $key;
+foreach ($weather_url as $key ) {
+	if($key == 'weather' ){
+		foreach ($key as $key2 =>$value) {
+			if($key2 == 'icon'){
+				$icon = "http://openweathermap.org/img/w/".$value.".png";
+			}
+		}
+		array_push($opendata, $icon);
+	}
 }
 
-// echo json_encode($weather_url);
+ echo json_encode($opendata);
 
 
