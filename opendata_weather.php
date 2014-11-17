b3bd44279049a06b9de5728470c5733385879d71
Kintoki <s12113240@stu.edu.tw>
2014-11-17 23:57:16 +0800

    update

--- opendata_weather.php
+++ opendata_weather.php
@@ -1,7 +1,7 @@
 <?php
 header("Content-Type:application/json; ");
 require_once('loader.php');
-$weather_url = json_decode(file_get_contents( "http://api.openweathermap.org/data/2.5/weather?lat=".$POST['lat']."&lon=".$_POST[''] ),true);
+$weather_url = json_decode(file_get_contents( "http://api.openweathermap.org/data/2.5/weather?lat=".$POST['lat']."&lon=".$_POST['lon'] ),true);
 
 $opendata = array("map"=>array());
 
