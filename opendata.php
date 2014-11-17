8e01b08bbc75d17c4a90e5756a5947e218037968
Kintoki <s12113240@stu.edu.tw>
2014-11-17 22:37:59 +0800

    update

--- opendata.php
+++ opendata.php
@@ -7,7 +7,7 @@ $food_url = json_decode(file_get_contents( "http://data.coa.gov.tw/Service/OpenD
 // for ($i=0; $i < count($food_url) ; $i++) { 
 // 	unset($food_url[$i]['sc_id']);
 // }
-
+unset($food_url[0]['sc_id']);
 echo json_encode($food_url[0]);
 
 
