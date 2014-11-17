175db0a63e98e54e89296413129bf32a47260500
Kintoki <s12113240@stu.edu.tw>
2014-11-18 01:28:45 +0800

    wea

--- opendata_weather.php
+++ opendata_weather.php
@@ -1,7 +1,7 @@
 <?php
 header("Content-Type:application/json; ");
 require_once('loader.php');
-$weather_url = json_decode(file_get_contents( "http://api.openweathermap.org/data/2.5/weather?lat=".$_GET['lat']."&lon=".$_GET['lon'] ),true);
+$weather_url = json_decode(file_get_contents( "http://api.openweathermap.org/data/2.5/weather?lat=".$_POST['lat']."&lon=".$_POST['lon'] ),true);
 
 
 	
