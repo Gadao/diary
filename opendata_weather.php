1242dbb7ddd89de83325a9b4717fe556159b2c7d
Kintoki <s12113240@stu.edu.tw>
2014-11-17 23:23:34 +0800

    update

--- opendata_weather.php
+++ opendata_weather.php
@@ -4,18 +4,18 @@ require_once('loader.php');
 $weather_url = json_decode(file_get_contents( "http://api.openweathermap.org/data/2.5/weather?lat=".$_GET['lat']."&lon=".$_GET['lon'] ),true);
 
 $opendata = array();
-foreach ($weather_url as $key ) {
-	echo $key;
-	if($key == 'weather' ){
-		foreach ($key as $key2 =>$value) {
-			// if($key2 == 'icon'){
-			// 	$icon = "http://openweathermap.org/img/w/".$value.".png";
-			// 	array_push($opendata, $icon);
-			// }
-			echo $key2;
-		}
+foreach ($weather_url as $key => $value) {
+	// if($key == 'weather' ){
+	// 	foreach ($key as $key2 =>$value) {
+	// 		// if($key2 == 'icon'){
+	// 		// 	$icon = "http://openweathermap.org/img/w/".$value.".png";
+	// 		// 	array_push($opendata, $icon);
+	// 		// }
+	// 		echo $key2;
+	// 	}
 		
-	}
+	// }
+	echo $key."  ".$value;
 }
 
  echo json_encode($opendata);
