cff5465950819ad112cd02cc624e0fe7d121127d
Kintoki <s12113240@stu.edu.tw>
2014-11-17 22:46:49 +0800

    update

--- opendata.php
+++ opendata.php
@@ -3,11 +3,11 @@ header("Content-Type:application/json; ");
 require_once('loader.php');
 $food_url = json_decode(file_get_contents( "http://data.coa.gov.tw/Service/OpenData/CC/FoodData.aspx" ));
 
-
+$opendata = array();
 // for ($i=0; $i < count($food_url) ; $i++) { 
 // 	unset($food_url[$i]['sc_id']);
 // }
 // unset($food_url[0]['sc_id']);
-echo json_encode($food_url[0]);
+echo $food_url[0]['type'];
 
 
