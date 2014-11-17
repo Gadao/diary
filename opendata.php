52773897d74fad531a2621f914707f5c62c49a39
Kintoki <s12113240@stu.edu.tw>
2014-11-17 22:32:34 +0800

    update

--- opendata.php
+++ opendata.php
@@ -3,6 +3,9 @@ header("Content-Type:application/json; ");
 require_once('loader.php');
 $food_url = json_decode(file_get_contents( "http://data.coa.gov.tw/Service/OpenData/CC/FoodData.aspx" ));
 
-print_r($food_url);
+foreach ($food_url as $key => $value) {
+	unset($food_url['sc_id']);
+}
+echo json_encode($food_url);
 
 
